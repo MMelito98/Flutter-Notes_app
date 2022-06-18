a5fc9a1e160c7df279d5fb4440775aa6eedc5b83
Michael Melito <m.melito98@gmail.com>
2022-06-18 17:22:40 -0400

    Adding loading dialog

--- lib/services/auth/bloc/auth_state.dart
+++ lib/services/auth/bloc/auth_state.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart'
   show immutable;
 import 'package:primo/services/auth/auth_user.dart';
+import 'package:equatable/equatable.dart';
 
 @immutable
 abstract class AuthState{
@@ -25,12 +26,15 @@ class AuthStateNeedsVerification extends AuthState{
   const AuthStateNeedsVerification();
 }
 
-class AuthStateLoggedOut extends AuthState{
+class AuthStateLoggedOut extends AuthState with EquatableMixin{
   final Exception? exception;
   final bool isLoading;
   const AuthStateLoggedOut({
     required this.exception,
     required this.isLoading,
   });
+
+  @override
+  List<Object?> get props => [exception, isLoading];
 }
 
